e5a8e0b08330398f91bd76ed4044f8f291d099e1
Supre <59318031+Supre-le-sucre@users.noreply.github.com>
2022-10-22 22:51:31 +0200

    Courbe du dragon
    docstring, deplace_diag_rad

--- Activité-6/ACT6.py
+++ Activité-6/ACT6.py
@@ -95,18 +95,37 @@ def spirale(ori: Point, dec: float, n: int) -> Courbe:
 # show_image(image_courbe(spirale(O, 0.01, 400)))
 
 def convertion_radian(angle: float) -> float:
+    """Converti l'angle en degré vers le radian"""
     return angle*math.pi/180
 
+assert convertion_radian(30) == math.pi/6
+assert convertion_radian(60) == math.pi/3
+assert convertion_radian(120) == 2*math.pi/3
+
 def convertion_angle(rad: float) -> float:
+    """Converti l'angle en radian vers le degré"""
     return rad/math.pi * 180
 
+assert convertion_angle(math.pi) == 180
+assert convertion_angle(math.pi/4) == 45
+assert convertion_angle(3*math.pi/2) == 270
+
 def deplace_diag(p: Point, angle: float, to: float) -> Point:
-    """Effectue le décalage diagonale du point fourni dans la direction souhaitée
+    """Effectue le décalage diagonale du point fourni dans l'angle en degré souhaité
     L'angle donnée est dans le sens trigonométrique"""
     radian_angle: float = convertion_radian(angle)
     a: float = to * math.sin(radian_angle)
     b: float = to * math.cos(radian_angle)
     return deplace(deplace(p, "H", a), "D", b)
+
+
+def deplace_diag_rad(p: Point, angle: float, to: float) -> Point:
+    """Effectue le décalage diagonale du point fourni dans l'angle en radian souhaité
+    L'angle donnée est dans le sens trigonométrique"""
+    a: float = to * math.sin(angle)
+    b: float = to * math.cos(angle)
+    return deplace(deplace(p, "H", a), "D", b)
+    
 
 def angle_seg(A: Point, B: Point) -> float:
     """Détermine l'angle en radian formé par la droite AB par rapport à l'axe x dans le sens trigonométrique"""
@@ -116,38 +135,45 @@ def angle_seg(A: Point, B: Point) -> float:
     I: Point = (xB, yA)
     l_AI: float = longueur_euclidienne(A, I)
 
-    # Evalue la position de B par rapport à A
+    # Evaluation de la position de B par rapport à A dans le cercle trigonométrique
 
+    # On met A en tant qu'origine du repère pour le calcul
     x: float = xA*(-1)
     y: float = yA*(-1)
 
+    # On vérifie la poisition de B par rapport à A
     a: float = xB + x
     b: float = yB + y
 
     if(a == 0 and b == 0):
+        # L'angle est nul
         return 0
 
     if(a <= 0 and b >= 0):
+        # second quartier du cercle
         return math.pi - math.acos(l_AI/ longueur_euclidienne(A, B))
 
     if(a <= 0 and b <= 0):
+        # troisième quartier du cercle
         return math.pi + math.acos(l_AI/ longueur_euclidienne(A, B))
 
     if(a >= 0 and b <= 0):
+        # quatrième quartier du cercle
         return  - math.acos(l_AI/ longueur_euclidienne(A, B))
 
+    # Premier quartier du cercle
     return math.acos(l_AI/ longueur_euclidienne(A, B))
 
 
 def fragment(A: Point, B: Point) -> Courbe:
-    """Fragmente la segment donnée à la manière du fractale de Koch"""
+    """Fragmente le segment donné à la manière du fractale de Koch"""
     c: Courbe = [A]
-    angle: float = convertion_angle(angle_seg(A, B))
+    angle: float = angle_seg(A, B)
     taille: float = longueur_euclidienne(A, B)
-    p1: Point = deplace_diag(A, angle, 1/3*taille)
+    p1: Point = deplace_diag_rad(A, angle, 1/3*taille)
     c.append(p1)
-    c.append(deplace_diag(p1, angle + 60, 1/3*taille))
-    c.append(deplace_diag(A, angle, 2/3*taille))
+    c.append(deplace_diag_rad(p1, angle + convertion_radian(60), 1/3*taille))
+    c.append(deplace_diag_rad(A, angle, 2/3*taille))
     c.append(B)
 
     return c
@@ -162,17 +188,25 @@ def append_list(source: List[T], destination: List[T]) -> List[T]:
         source.append(e)
     return source
 
-def courbe_flocon(p: Point, r: float, n: int) -> Courbe:
+def triangle_de_rayon_r(p: Point, r: float) -> Courbe:
+    """Renvoie la courbe correspondant au triangle de rayon r"""
     # Traçage des points du triangle donné
 
     S: Point = deplace(p, "H", r) # Sommet
     A: Point = deplace_diag(p, 210, r) # Point inférieur gauche
     B: Point = deplace_diag(p, -30, r) # Point inférieur droit
 
+    return [A, S, B, A]
+
+def courbe_flocon(p: Point, r: float, n: int) -> Courbe:
+    """Préconditions r >= 0 and n >= 0
+    Retourne le flocon de Koch avec le rayon du triangle et le nombre d'itération indiquée"""
+    
+
     P1: Point
     P2: Point
 
-    c: Courbe = [A, S, B, A]
+    c: Courbe = triangle_de_rayon_r(p, r)
     c_prev: Courbe
 
     i: int
@@ -187,5 +221,42 @@ def courbe_flocon(p: Point, r: float, n: int) -> Courbe:
         
     return c
 
-show_image(image_courbe(courbe_flocon(O, 0.8, 6)))
-        
+# show_image(image_courbe(courbe_flocon(O, 0.8, 6)))
+
+# Suggestion 3
+
+def courbe_dragon(A: Point, B: Point, n: int) -> Courbe:
+    """Préconditions n >= 0
+    Retourne la coubre correspondant à la courbe du dragon à partir du segment [AB]""" 
+    c: Courbe = [A, B]
+    c_prev: Courbe
+    taille: float
+    P1: Point
+    P2: Point
+    angle: float
+    i: int
+    j: int
+    for i in range(1, n+1):
+        c_prev = c
+        c = []
+        for j in range(0, len(c_prev)-1):
+
+            P1 = c_prev[j]
+            P2 = c_prev[j+1]
+
+            taille = longueur_euclidienne(P1, P2)
+            angle = angle_seg(P1, P2)
+            
+            c.append(P1)
+            if(j%2 == 0):
+                c.append(deplace_diag_rad(P1, angle + math.pi/4, math.sqrt(1/2*taille**2)))
+            else:
+                c.append(deplace_diag_rad(P1, angle - math.pi/4, (math.sqrt(1/2*taille**2))))
+        c.append(c_prev[-1])
+    return c
+
+# show_image(overlay(image_courbe(courbe_dragon((-0.5, 0), (0.5, 0), 2)), (image_courbe(courbe_dragon((-0.5, 0), (0.5, 0), 1)))))
+
+# show_image(image_courbe(courbe_dragon((-0.5, 0), (0.5, 0), 14)))
+            
+
