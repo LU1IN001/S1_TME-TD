c35230a3c689800ec62fd410b9c792a7c85a5cfb
Supre <59318031+Supre-le-sucre@users.noreply.github.com>
2022-10-28 17:21:12 +0200

    Suggestion 2 et 3

--- Activité-7/ACT7.py
+++ Activité-7/ACT7.py
@@ -1,3 +1,5 @@
+# Wilhem Blondel
+import random
 
 Coord = Tuple[int, int]
 Dir = str
@@ -84,7 +86,7 @@ def est_solution(la: Laby, c: Coord, ch: List[Dir]) -> bool:
         return False
 
     # Vérification du chemin possible
-    if not chemin_possible(la, c, ch):
+    elif not chemin_possible(la, c, ch):
         return False
 
     # Vérification de la position finale de c
@@ -94,15 +96,134 @@ def est_solution(la: Laby, c: Coord, ch: List[Dir]) -> bool:
     if state_fin != "SORTIE":
         return False
 
-    # Si on a jamais return, alors le cehmin est solution
-    return True
+    # Si on a jamais return, alors le chemin est solution
+    else:
+        return True
 
 assert est_solution(laby1 , (0, 1), ["S", "E", "N"])
 assert est_solution(laby1 , (0, 1), ["S", "E", "O", "E", "N"])
 assert not est_solution(laby1 , (0, 0), ["E", "N"])
 assert not est_solution(laby1 , (0, 1), ["S", "E"])
 assert not est_solution(laby1 , (0, 1), ["E"])
+
+# Suggestion 2
+
+def random_int(n: int) -> int:
+    """Préconditions n >= 0
+    Renvoie un entier aléatoire entre 0 et le nombre donné exclus"""
+    return int(n * random.random())
+
+assert 0 <= random_int(4) < 4
+assert 0 <= random_int(65) < 65
+assert 0 <= random_int(32) < 32
+
+def random_path(c: Case) -> Dir:
+    """Préconditions case a une sortie
+    Renvoie une direction aléatoire possible à prendre dans la case c"""
+    n, e, s, o, _ = c
+    dir_possible: List[Dir] = []
     
+    if n:
+        dir_possible.append("N")
+    if e:
+        dir_possible.append("E")
+    if s:
+        dir_possible.append("S")
+    if o:
+        dir_possible.append("O")
+
+    return dir_possible[random_int(len(dir_possible))]
 
 
-        
+def coord_depart(la: Laby, c: Coord) -> Coord:
+    """Préconditions, la contient une entrée, c est dans le labyrinthe
+    Trouve les coordonnées de départ de la"""
+
+    # On veut faire une récursivité inverse ici
+    # Puisque l'entrée se trouvera plus souvent en début de liste
+    x, y = c
+    
+    _, _, _, _, state = la[x][y]
+    
+    if state == "ENTREE":
+        return c
+    else:
+        return coord_depart(la, (x, y+1))
+
+assert coord_depart(laby1, (0, 0)) == (0, 1)
+
+
+def marche_aleatoire(la: Laby, chemin: List[Dir], c_prev: Optional[Coord]) -> Optional[List[Dir]]:
+    """Renvoie une chemin aléatoire solution du labyrinthe"""
+
+    # On préfère ici stocker les éléments des coordonnée pour ne pas vaoir utiliser les fonctions récursives de déplacement
+    # Qui prendrait trop de place sur la pile
+    
+    res_next: List[Dir] = chemin[:]
+
+    c: Coord
+
+    if(c_prev == None):
+        c = coord_depart(la, (0, 0)) # Ceci prend beaucoup de place dans la pile pour de grand labyrinthe
+                                     # Le mieux est de fournir les coordonnées d'entrer du labyrinthe avant l'exécution première
+    else:
+        c = c_prev # La valeur n'est jamais None
+    
+    x, y = c
+    if len(chemin) > 950:
+        # Surcharge de la pile maîtrisé
+        return None
+    
+    _, _, _, _, state = la[x][y]
+    
+    if state == "SORTIE":
+        return chemin
+
+    else:
+        dir_alea: Dir = random_path(la[x][y])
+        res_next.append(dir_alea)
+        return marche_aleatoire(la, res_next, deplace(c, dir_alea))
+
+# Suggestion 3
+
+def marche_droite(la: Laby, chemin: List[Dir], c_prev: Optional[Coord]) -> Optional[List[Dir]]:
+    """Renvoie une chemin aléatoire solution du labyrinthe"""
+
+    # On préfère ici stocker les éléments des coordonnée pour ne pas vaoir utiliser les fonctions récursives de déplacement
+    # Qui prendrait trop de place sur la pile
+    
+    res_next: List[Dir] = chemin[:]
+
+    c: Coord
+
+    if(c_prev == None):
+        c = coord_depart(la, (0, 0)) # Ceci prend beaucoup de place dans la pile pour de grand labyrinthe
+                                     # Le mieux est de fournir les coordonnées d'entrer du labyrinthe avant l'exécution première
+    else:
+        c = c_prev # La valeur n'est jamais None
+    
+    x, y = c
+    if len(chemin) > 950:
+        # Surcharge de la pile maîtrisé
+        return None
+    
+    _, _, _, _, state = la[x][y]
+    
+    if state == "SORTIE":
+        return chemin
+
+    else:
+        if deplace_possible(la, c, "E"):
+            res_next.append("E")
+            return marche_droite(la, res_next, deplace(c, "E"))
+        elif deplace_possible(la, c, "N"):
+            res_next.append("N")
+            return marche_droite(la, res_next, deplace(c, "N"))
+        elif deplace_possible(la, c, "O"):
+            res_next.append("O")
+            return marche_droite(la, res_next, deplace(c, "O"))
+        else:
+            res_next.append("S")
+            return marche_droite(la, res_next, deplace(c, "S"))
+
+assert marche_droite(laby1, [], None) == ['S', 'E', 'N']
