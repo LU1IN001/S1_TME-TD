38ed9f8c985a2db7cddb4f9b33f130820521ed4b
Supre-le-sucre <wilhemblondel@gmail.com>
2022-12-09 18:09:35 +0100

    doesn't work dk why, not gonna fix it

--- TP10/ex12.3.py
+++ TP10/ex12.3.py
@@ -27,15 +27,16 @@ def tourner_sous_matrice(m: Matrice, x: int, y: int, k: int) -> None:
         j: int
         for j in range(len(values)):
             # Gestion des valeurs sur la même ligne
-            temp: int = m[ligne][colonne+j]
+            temp = m[ligne][colonne+j]
             m[ligne][colonne+j] = values[j]
             values[j] = temp
         ligne = ligne + cst # Passage à la ligne suivante (vers bas puis haut)
-        for j in range(len(values)):
+        o: int
+        for o in range(len(values)):
             # Gestion des valeurs sur la même colonne
-            temp: int = m[ligne][colonne+j]
-            m[ligne][colonne+j] = values[j]
-            values[j] = temp
+            temp = m[ligne][colonne+o]
+            m[ligne][colonne+o] = values[o]
+            values[o] = temp
         colonne = colonne - cst # Passage à la colonne suivante (vers la gauche puis droite)
 
 m0: Matrice = [[0, 0, 0, 0],
@@ -52,8 +53,70 @@ m1 : Matrice = [[ 1, 2, 3, 4, 0, 0, 0, 0],
                 [ 0, 0, 0, 0, 0, 0, 0, 0],
                 [ 0, 0, 0, 0, 0, 0, 0, 0]]
 
-tourner_sous_matrice(m1, 0, 0, 4)
-tourner_sous_matrice(m0, 1, 1, 2)
+assert tourner_sous_matrice(m0, 1, 1, 2) == None
+assert m0 == [[0, 0, 0, 0], [0, 3, 1, 0], [0, 4, 2, 0], [0, 0, 0, 0]]  
+assert tourner_sous_matrice(m1, 0, 0, 4) == None
+assert m1 == [[9, 10, 1, 2, 0, 0, 0, 0], [13, 14, 5, 6, 0, 0, 0, 0], [11, 12, 3, 4, 0, 0, 0, 0], [15, 16, 7, 8, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
 
-print(m1)
-print(m0)
+
+def tourner_matrice(m: Matrice, k: int) -> None:
+    """***Procédure***
+    Fait tourner la matrice m en fonction d'une sous matrice de taille k
+    Tourner à 90° par quadrant"""
+    nb_iter: int = (len(m)//k)**2 # Calcul le nombre d'itération
+                                          # L'élévation au carré permet de prendre en compte l'intégralité de la matrice
+    
+    x: int = 0
+    y: int = 0
+    i: int
+    for i in range(1, nb_iter+2):
+        tourner_sous_matrice(m, x, y, k)
+        if i%(len(m)//k) != 0:
+            x = x + k
+        else:
+            x = 0
+            y = y + k
+
+m2 : Matrice = [[0,0,0,0],
+                [0,1,2,0],
+                [0,3,4,0],
+                [0,0,0,0]]
+
+m3 : Matrice = [[0,0,0,0],
+                [0,1,2,0],
+                [0,3,4,0],
+                [0,0,0,0]]
+
+m4 : Matrice = [[0,0,0,0],
+                [0,1,2,0],
+                [0,3,4,0],
+                [0,0,0,0]]
+
+assert tourner_matrice(m2, 2) == None
+assert m2 == [[0, 0, 2, 0], [1, 0, 0, 0], [0, 0, 0, 4], [0, 3, 0, 0]]
+assert tourner_matrice(m3, 1) == None
+assert m3 == [[0, 0, 0, 0], [0, 1, 2, 0], [0, 3, 4, 0], [0, 0, 0, 0]]
+assert tourner_matrice(m4, 4) == None
+assert m4 == [[0, 3, 0, 0], [0, 0, 0, 1], [4, 0, 0, 0], [0, 0, 2, 0]]
+
+def tourner(m: Matrice) -> None:
+    i: int = 2
+    while i <= len(m):
+            print(i)
+            tourner_matrice(m, i)
+            i = i*2
+
+m5: Matrice = [[1, 2],
+               [3, 4]]
+
+m6: Matrice = [[l for l in range(2 ** 3 * k, 2 ** 3 * (k + 1))]
+                for k in range(2 ** 3)]
+
+assert m6 == [[0, 1, 2, 3, 4, 5, 6, 7], [8, 9, 10, 11, 12, 13, 14, 15], [16, 17, 18, 19, 20, 21, 22, 23], [24, 25, 26, 27, 28, 29, 30, 31], [32, 33, 34, 35, 36, 37, 38, 39], [40, 41, 42, 43, 44, 45, 46, 47], [48, 49, 50, 51, 52, 53, 54, 55], [56, 57, 58, 59, 60, 61, 62, 63]]
+
+assert tourner(m5) == None
+assert m5 == [[3, 1], [4, 2]]
+
+assert tourner(m6) == None
+print(m6)
+assert m6 == [[56, 48, 40, 32, 24, 16, 8, 0], [57, 49, 41, 33, 25, 17, 9, 1], [58, 50, 42, 34, 26, 18, 10, 2], [59, 51, 43, 35, 27, 19, 11, 3], [60, 52, 44, 36, 28, 20, 12, 4], [61, 53, 45, 37, 29, 21, 13, 5], [62, 54, 46, 38, 30, 22, 14, 6], [63, 55, 47, 39, 31, 23, 15, 7]]
